cd51ccb5f38b2b30fcbd5799de1d906544f16aba
WendingLin <lycoris000@126.com>
2019-05-16 06:10:41 -0400

    fastcommit.sh

--- 5.cpp
+++ 5.cpp
@@ -7,41 +7,31 @@ public:
       return s;
     string sub = "";
     int len = s.size();
-    bool **dp = new bool *[len]();
-    for (auto i = 0; i < len; i++)
-      dp[i] = new bool[len]();
+    int max_len = 0;
+    vector<vector<bool>> dp(len, vector<bool>(len, 0));
 
     for (auto i = 0; i < len; i++) {
       dp[i][i] = true;
-      sub = s[i];
-    }
-
-    for (auto i = 0; i < len; i++) {
-      for (auto j = 0; j < len; j++) {
-        if (i == j) {
-          continue;
-        } else if (i + 1 == j && s[i] == s[j]) {
-          dp[i][j] = true;
-          sub = s.substr(i, 2);
-        } else
-          dp[i][j] = false;
-      }
     }
 
     bool *flag = new bool[len]();
     flag[0] = true;
-    flag[1] = true;
-    for (int i = 2; i < len; i++) {
+    int pos = 0;
+    for (int i = 1; i < len; i++) {
       for (int j = 0; j < len - i; j++) {
-        if (dp[j + 1][j + i - 1] && s[j] == s[j + i]) {
+        if ((dp[j + 1][j + i - 1] || i == 1) && s[j] == s[j + i]) {
           dp[j][j + i] = true;
-          sub = s.substr(j, i + 1);
           flag[i] = true;
+          pos = j;
         }
       }
-      if (flag[i - 1] == false && flag[i - 2] == false)
+      if (flag[i])
+        max_len = i;
+      if (flag[i - 1] == false && i != 1 && flag[i - 2] == false)
         break;
     }
+    for (int i = pos; i < pos + max_len + 1; ++i)
+      sub += s[i];
     return sub;
   }
 };
