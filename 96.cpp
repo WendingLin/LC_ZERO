c769e88195b16649585e3a298c756e1b3ae07faf
WendingLin <lycoris000@126.com>
2019-05-16 06:46:40 -0400

    fastcommit.sh

---
+++ 96.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+  int numTrees(int n) {
+    if (n == 0 || n == 1)
+      return n;
+    int *dp = new int[n + 1]();
+    dp[0] = 1;
+    for (int i = 1; i < n + 1; i++) {
+      for (int j = 0; j < i; j++) {
+        dp[i] += dp[j] * dp[i - j - 1];
+      }
+    }
+    return dp[n];
+  }
+};
