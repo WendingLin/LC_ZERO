31ee5397d9378df3f6b381e046817b4d0978ad98
WendingLin <lycoris000@126.com>
2019-05-20 03:56:34 -0400

    41

--- 41.java
+++ 41.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution { // test
   public int firstMissingPositive(int[] nums) {
     for (int i = 0; i < nums.length; i++) {
       while (nums[i] >= 1 && nums[i] != i + 1 && nums[i] <= nums.length &&
