2c39972f79b2c3236574f6f22d873b90af9c3c63
WendingLin <lycoris000@126.com>
2019-05-20 00:49:08 -0400

    fastcommit.sh

---
+++ 26.java
@@ -0,0 +1,15 @@
+class Solution {
+  public int removeDuplicates(int[] nums) {
+    if (nums.length == 0)
+      return 0;
+    int count = 0;
+    for (int i = 0; i < nums.length - 1; i++) {
+      if (nums[i + 1] == nums[i]) {
+        continue;
+      }
+      nums[count++] = nums[i];
+    }
+    nums[count++] = nums[nums.length - 1];
+    return count;
+  }
+}
