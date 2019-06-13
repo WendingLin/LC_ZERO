e5c1c871ddaaaa2040cde3b3c52bd99b23e3193c
WendingLin <lycoris000@126.com>
2019-06-12 23:22:10 -0400

    242

---
+++ 242.java
@@ -0,0 +1,17 @@
+class Solution {
+  public boolean isAnagram(String s, String t) {
+    if (s.length() != t.length())
+      return false;
+    int[] count = new int[26];
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+      count[c - 'a']++;
+    }
+    for (int i = 0; i < t.length(); i++) {
+      char c = t.charAt(i);
+      if (--count[c - 'a'] < 0)
+        return false;
+    }
+    return true;
+  }
+}
