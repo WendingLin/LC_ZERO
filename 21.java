36bc1f7d34825248310dcbb641d57353d955268e
WendingLin <lycoris000@126.com>
2019-06-20 22:49:48 -0400

    21

---
+++ 21.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+  public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    if (l1 == null && l2 == null) {
+      return null;
+    } else if (l1 == null && l2 != null) {
+      return l2;
+    } else if (l1 != null && l2 == null) {
+      return l1;
+    }
+
+    ListNode head = new ListNode(-1);
+    ListNode curr = head;
+
+    while (l1 != null && l2 != null) {
+      if (l1.val > l2.val) {
+        curr.next = l2;
+        l2 = l2.next;
+      } else {
+        curr.next = l1;
+        l1 = l1.next;
+      }
+      curr = curr.next;
+    }
+    if (l1 == null && l2 != null) {
+      while (l2 != null) {
+        curr.next = l2;
+        l2 = l2.next;
+        curr = curr.next;
+      }
+    } else if (l1 != null && l2 == null) {
+      while (l1 != null) {
+        curr.next = l1;
+        l1 = l1.next;
+        curr = curr.next;
+      }
+    }
+
+    return head.next;
+  }
+}
