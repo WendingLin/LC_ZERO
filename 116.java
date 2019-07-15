2b28f269a94b768be3b793a549922d0474fb9f80
WendingLin <lycoris000@126.com>
2019-07-15 00:30:50 -0400

    116

---
+++ 116.java
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val,Node _left,Node _right,Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+class Solution {
+    private Queue<Node> queue= new LinkedList<Node>();
+    public Node connect(Node root) {
+        if(root==null)
+            return root;
+        queue.offer(root);
+        while(queue.isEmpty()==false){
+            int cnt = queue.size();
+            for(int i = 0; i<cnt; i++){
+                Node temp = queue.poll();
+                if(temp.left!=null && temp.right!=null){
+                    queue.offer(temp.left);
+                    queue.offer(temp.right);
+                } 
+                if(i!=cnt-1){
+                    temp.next = queue.peek();
+                }
+            }
+        }
+        return root;
+    }
+}
