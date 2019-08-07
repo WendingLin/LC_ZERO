ca8c3c30c2825309d24c38967c9f9c2b612e8918
WendingLin <lycoris000@126.com>
2019-08-07 00:09:57 -0400

    3

--- 3.java
+++ 3.java
@@ -1,1 +1,19 @@
-1
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if(s==null || s.length()==0) return 0;
+        
+        int[] lastpos = new int[256];
+        Arrays.fill(lastpos, -1);
+        int res = 0;
+        int left = -1;
+        for(int i = 0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(lastpos[c]>=left){
+                left = lastpos[c];
+            }
+            res = Math.max(res, i-left);
+            lastpos[c] = i;
+        }
+        return res;
+    }
+}1
